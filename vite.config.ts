d89814b2a29f310a81f5aa8e419fae052c13585f
Nikita <fedosovnikita007@gmail.com>
2025-06-01 20:04:22 +0300

    Ok, lets go

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5555, // change to your desired port
+    host: '0.0.0.0'
+  },
   plugins: [
     vue(),
     vueDevTools(),
